ca8a747fd201e140f00d85d465937d4ec142c017
grez911 <grez911@gmail.com>
2018-04-04 20:27:48 +0300

    Changed e-mail

--- js/main_faq.js
+++ js/main_faq.js
@@ -3,11 +3,11 @@
 function compose_email_address()
 {
     var email = 'grez<span class="invisible">gibber1</span>';
-    email += '9<!--gibber2-->11@';
+    email += '<!--gibber2-->@';
     email += '<span class="invisible">gibber3</span>';
-    email += 'gma<span class="invisible">gibber4<span class="invisible">';
+    email += 'firema<span class="invisible">gibber4<span class="invisible">';
     email += 'gibber5</span></span>';
-    email += 'il.com';
+    email += 'il.cc';
     document.getElementById("email").innerHTML = email;
 }
 
