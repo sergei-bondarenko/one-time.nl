1e69bca46949bf15fc7efe1f7a8371cde14b54aa
Sergei Bondarenko <grez911@gmail.com>
2024-08-24 20:22:08 +0100

    Update email.

--- js/main_faq.js
+++ js/main_faq.js
@@ -2,12 +2,7 @@
 
 function compose_email_address()
 {
-    var email = 'grez<span class="invisible">gibber1</span>';
-    email += '<!--gibber2-->911@';
-    email += '<span class="invisible">gibber3</span>';
-    email += 'gma<span class="invisible">gibber4<span class="invisible">';
-    email += 'i</span></span>';
-    email += 'l.com';
+    var email = 'example@email.com';
     document.getElementById("email").innerHTML = email;
 }
 
